f5a7ae5ead0c3a3bee021d682d7bfe3c740e485e
Pol Gubau <gubaupol@gmail.com>
2022-08-03 20:39:01 +0200

    ft: folder display

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
-
+import Title from "./components/Title";
 function App() {
-  return <div className="App">Gerard Martinez</div>;
+  return (
+    <div className="App">
+      <Title />
+    </div>
+  );
 }
 
 export default App;
