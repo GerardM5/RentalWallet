285b775b015314772016f6faedf6fd86a5157225
Pol Gubau <gubaupol@gmail.com>
2022-08-04 08:56:45 +0200

    ft: uploaded color palette

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,19 +1,10 @@
-import React from 'react'
-import "./Nav.css"
-import {Link} from 'react-router-dom'
+import React from "react";
+import "./Nav.css";
 export default function Nav() {
- 
   return (
-    <nav className='container'>
-        <div className='navSide' >
-            <Link to ='/gerard'>
-              logo
-            </Link>
-            
-        </div>
-        <div className='navSide'>
-            usuario
-        </div>
+    <nav className="container">
+      <div className="navSide">logo</div>
+      <div className="navSide">usuario</div>
     </nav>
-  )
+  );
 }
