e7885c7e865fbba8ce3f34c26d6d136038b0040b
Pol Gubau <gubaupol@gmail.com>
2022-08-07 19:21:41 +0200

    app component with routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import "./styles/App.css";
-
-// import RentalList from "./components/RentalList";
 import Nav from "./components/Nav";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Rental from "./pages/Rental";
+import Profile from "./pages/Profile";
+import Login from "./pages/Login";
+import About from "./pages/About";
+import Home from "./pages/Home";
+
 function App() {
   return (
     <div className="App">
       <Nav />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/rental" element={<Rental />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
