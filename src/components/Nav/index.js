3be31064cc16eb0d961f9a079efad521f8fb2c4d
Pol Gubau <gubaupol@gmail.com>
2022-08-07 19:22:23 +0200

    image logo on nav

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,11 +1,25 @@
 import React from "react";
 import "./Nav.css";
-import LargeLogo from "components/Logos/LargeLogo";
+
+import logo from "src/assets/logo/LargeLogo.png";
+import { Link } from "react-router-dom";
+
 export default function Nav() {
   return (
     <nav className="container">
-      <LargeLogo />
-      <div className="navSide">usuario</div>
+      <div className="LogoContainer">
+        <img src={logo} alt="Logo" className="LogoImage" />
+      </div>
+      <div>
+        <Link to="/profile">
+          <img
+            src="https://api.multiavatar.com/Starcrasher.svg
+"
+            alt="Profile"
+            className="ProfileImage"
+          />
+        </Link>
+      </div>
     </nav>
   );
 }
