8079a1d83b591caa35ff784dce06506c568c4877
Pol Gubau <gubaupol@gmail.com>
2022-08-24 10:20:04 +0200

    ft: deleted all unused pages

--- src/pages/ProfilePage.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { useParams } from "react-router-dom";
-
-export default function ProfilePage() {
-  const { id } = useParams();
-  return <div>ProfilePage del id {id}</div>;
-}
