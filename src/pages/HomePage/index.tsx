1301d6b0e7879f25ecdfccd7878d43188867970c
Pol Gubau <gubaupol@gmail.com>
2022-08-24 10:20:24 +0200

    ft: HomePage created

---
+++ src/pages/HomePage/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./HomePage.css";
+import AppLayout from "components/Layout/AppLayout/AppLayout";
+export default function HomePage() {
+  return (
+    <section>
+      <AppLayout>Home Page</AppLayout>
+    </section>
+  );
+}
