2a57bc88c666afabd434a8a4a9f8619ffbccc925
Pol Gubau <gubaupol@gmail.com>
2022-08-24 10:47:15 +0200

    nav created

--- src/components/Layout/AppLayout/AppLayout.tsx
+++ src/components/Layout/AppLayout/AppLayout.tsx
@@ -1,10 +1,32 @@
 import React from "react";
 import "./AppLayout.css";
+import { FaSearch } from "react-icons/fa";
+
 export default function AppLayout({ children }) {
   return (
     <div className="container">
       <section className="leftSection">NAV</section>
-      <section className="rightSection"> {children}</section>
+      <section className="rightSection">
+        <nav className="navContainer">
+          <div className="navRight">
+            <div className="searchContainer">
+              <input
+                type="text"
+                placeholder="Busca tu anuncio perfecto"
+                className="inputSearch"
+              />
+              <button className="btnSearch">
+                <span className="searchIcon">
+                  <FaSearch />
+                </span>
+                <span className="searchText">Buscar</span>
+              </button>
+            </div>
+          </div>
+          <div className="navLeft">ICONS</div>
+        </nav>
+        {children}
+      </section>
     </div>
   );
 }
