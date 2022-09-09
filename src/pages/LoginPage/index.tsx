a8f2741eb5628251aabf635b7ac011627c9d93e1
Pol Gubau <gubaupol@gmail.com>
2022-09-09 08:16:02 +0200

    Update index.tsx

--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import "./LoginPage.css";
 import AppLayout from "components/Layout/AppLayout/AppLayout";
 export default function LoginPage() {
+        
   return (
     <section>
       <AppLayout>Login Page</AppLayout>
     </section>
   );
 }
+ 
